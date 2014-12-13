5c82e1e40ed9e5c2e6faaa3b03ef1ba025a469c5
Srikrishnan S <srikrishnans@sase.ssn.edu.in>
2014-12-13 16:13:49 +0530

    Creating the base templates for html pages

--- sase/mycraze/urls.py
+++ sase/mycraze/urls.py
@@ -4,4 +4,5 @@ from mycraze import views
 
 urlpatterns = patterns('',
 	#url(r'^$', views.index, name='index'),
+	url(r'^test/$', views.test, name='test'),
 )
--- sase/mycraze/views.py
+++ sase/mycraze/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def test(request):
+	context = {'message': 'The site is healthy'}
+	return render(request, 'mycraze/test.html', context)
