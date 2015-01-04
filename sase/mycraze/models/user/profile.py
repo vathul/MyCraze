487024e25058bd7be83d5d3c847c0e76e8a6840d
Srikrishnan S <srikrishnans@sase.ssn.edu.in>
2015-01-04 17:30:26 +0530

    Changing the user profile to upload the profile image to save it in a folder by the name of that user's id

--- sase/mycraze/models/user/profile.py
+++ sase/mycraze/models/user/profile.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+def get_upload_path(instance, filename):
+    return 'profile_image/%s/%s' % (instance.user.id, filename)
+
 class UserProfile(models.Model):
 	class Meta:
 		app_label = 'mycraze'
 
 	user = models.OneToOneField(User, related_name='userProfile')
 	description = models.CharField(max_length=200)
-	profile_image = models.ImageField(upload_to="profile_image/", null=True, blank=True)
+	profile_image = models.ImageField(upload_to=get_upload_path)
