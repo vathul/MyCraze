4aebc50502f294e190947741e1141033ed7faf06
Srikrishnan S <srikrishnans@sase.ssn.edu.in>
2015-01-05 18:32:10 +0530

    Refactoring the storage variable name

--- sase/mycraze/models/user/profile.py
+++ sase/mycraze/models/user/profile.py
@@ -3,7 +3,7 @@ from django.core.files.storage import FileSystemStorage
 from django.db import models
 from sase.settings import PROFILE_IMAGES_DIR
 
-fs = FileSystemStorage(location=PROFILE_IMAGES_DIR)
+file_store = FileSystemStorage(location=PROFILE_IMAGES_DIR)
 
 def get_upload_path(instance, filename):
     return 'profile_images/%s/%s' % (instance.user.id, filename)
@@ -14,4 +14,4 @@ class UserProfile(models.Model):
 
 	user = models.OneToOneField(User, related_name='userProfile')
 	description = models.CharField(max_length=200)
-	profile_image = models.ImageField(upload_to=get_upload_path, storage=fs)
+	profile_image = models.ImageField(upload_to=get_upload_path, storage=file_store)
