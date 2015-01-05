564f720e47291b3796d92b12eb96bcb27f7dd206
Srikrishnan S <srikrishnans@sase.ssn.edu.in>
2015-01-05 18:33:39 +0530

    Moving the submit_profile function from LoginView to UserView

--- sase/mycraze/views/LoginView.py
+++ sase/mycraze/views/LoginView.py
@@ -5,7 +5,6 @@ from django.contrib.auth.decorators import login_required
 from mycraze.models.form.profile import UserForm
 from mycraze.models.form.profile import UserProfileForm
 from mycraze.models.user.profile import UserProfile
-from mycraze.services.user import UserProfileService
 
 # Create your login views here.
 
@@ -25,16 +24,6 @@ def get_profile_completion_page(request):
         "userForm": userForm,
         "profileForm": profileForm,
     })
-
-def submit_profile(request):
-	userForm = UserForm(request.POST)
-	userProfileForm = UserProfileForm(request.POST, request.FILES)
-	print(request.FILES)
-	if userForm.is_valid() and userProfileForm.is_valid():
-		user = userForm.save(commit=False)
-		userProfile = userProfileForm.save(commit=False)
-		UserProfileService.save_user_profile(request.user, user, userProfile)
-	return HttpResponseRedirect('/mycraze/user-resume')
 
 def get_logout_page(request):
 	logout(request)
--- sase/mycraze/views/UserView.py
+++ sase/mycraze/views/UserView.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
+from django.conf import settings
 from django.contrib.auth.decorators import login_required
-
+from mycraze.models.form.profile import UserForm
+from mycraze.models.form.profile import UserProfileForm
+from mycraze.models.user.profile import UserProfile
+from mycraze.services.user import UserProfileService
 # Create your views here.
 
 @login_required
+def submit_profile(request):
+	userForm = UserForm(request.POST)
+	userProfileForm = UserProfileForm(request.POST, request.FILES)
+	print(request.FILES)
+	if userForm.is_valid() and userProfileForm.is_valid():
+		user = userForm.save(commit=False)
+		userProfile = userProfileForm.save(commit=False)
+		UserProfileService.save_user_profile(request.user, user, userProfile)
+	return HttpResponseRedirect('/mycraze/user-resume')
+
+@login_required
 def get_resume_page(request):
-	return render(request, 'mycraze/user-resume.html')
+	setting = {
+		'image_url': settings.PROFILE_IMAGES_URL,
+	}
+	print(setting['image_url'])
+	return render(request, 'mycraze/user-resume.html',setting)
