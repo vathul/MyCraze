7aa2caedf2323f60222e9d666f61db0fcf093ce1
Srikrishnan S <srikrishnans@sase.ssn.edu.in>
2015-01-20 13:06:17 +0530

    Adding models for experience items and section

--- sase/mycraze/models/user/__init__.py
+++ sase/mycraze/models/user/__init__.py
@@ -1,3 +1,5 @@
 from mycraze.models.user.profile import UserProfile
 from mycraze.models.user.sections import SummarySection
-__all__ = ['UserProfile','SummarySection','ContactSection']
+from mycraze.models.user.sections import ExperienceSection
+from mycraze.models.user.items import ExperienceItem
+__all__ = ['UserProfile','SummarySection','ExperienceSection','ExperienceItem','ContactSection']
