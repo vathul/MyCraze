733b4e91d8e39236ca6bc847db162033bcb2c822
Srikrishnan S <srikrishnans@sase.ssn.edu.in>
2015-03-03 19:43:55 +0530

    Using a list of stackoverflow profiles for the to be constructed chart.

--- sase/mycraze/static/mycraze/js/chart.js
+++ sase/mycraze/static/mycraze/js/chart.js
@@ -1,5 +1,5 @@
 //https://api.stackexchange.com/2.2/users/{id}/top-tags?site=stackoverflow
-renderTagsChart = function() {
+renderTagsChart = function(list) {
 
 	var chart = new Highcharts.Chart({
 		chart : {
@@ -18,7 +18,7 @@ renderTagsChart = function() {
 		yAxis : {
 			min : 0,
 			title : {
-				text : 'Rainfall (mm)'
+				text : 'Count / Score'
 			}
 		},
 
@@ -33,25 +33,25 @@ renderTagsChart = function() {
 		},
 		series : [
 				{
-					name : 'Tokyo',
+					name : 'Answer Count',
 					data : [ 49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6,
 							148.5, 216.4, 194.1, 95.6, 54.4 ]
 
 				},
 				{
-					name : 'New York',
+					name : 'Answer Score',
 					data : [ 83.6, 78.8, 98.5, 93.4, 106.0, 84.5, 105.0, 104.3,
 							91.2, 83.5, 106.6, 92.3 ]
 
 				},
 				{
-					name : 'London',
+					name : 'Question Count',
 					data : [ 48.9, 38.8, 39.3, 41.4, 47.0, 48.3, 59.0, 59.6,
 							52.4, 65.2, 59.3, 51.2 ]
 
 				},
 				{
-					name : 'Berlin',
+					name : 'Question Score',
 					data : [ 42.4, 33.2, 34.5, 39.7, 52.6, 75.5, 57.4, 60.4,
 							47.6, 39.1, 46.8, 51.1 ]
 
