3a8bb525f5217343e205d81c2e39356f327daaf3
Srikrishnan S <srikrishnans@sase.ssn.edu.in>
2015-03-13 14:24:01 +0530

    Adding the search by first or last name feature, linking it with the search form in the header

--- sase/mycraze/services/search.py
+++ sase/mycraze/services/search.py
@@ -1,7 +1,27 @@
-class UserProfileService:
-    def get_users_by_name(self):
+from django.contrib.auth.models import User
+from django.db.models import Q
+
+class UserSearchService:
+    
+    @staticmethod
+    def get_users_by_name(q):
+        user_list = None
+        if q == None:
+            user_list = User.objects.filter(Q(user_profile__isnull=False))
+        else:
+            user_list = User.objects.filter(Q(first_name__icontains=q) | Q(last_name__icontains=q))
+        return user_list
+    
+    @staticmethod
+    def get_users_by_skills():
+        print('skills')
         return ""
-    def get_users_by_skills(self):
-        return ""
-    def get_users(self):
-        return ""
+    
+    @staticmethod
+    def get_all_users(q):
+        user_list = None
+        if q == None:
+            user_list = User.objects.filter(Q(user_profile__isnull=False))
+        else:
+            user_list = User.objects.filter(Q(first_name__icontains=q) | Q(last_name__icontains=q))
+        return user_list
--- sase/mycraze/views/UserSearchView.py
+++ sase/mycraze/views/UserSearchView.py
@@ -2,22 +2,29 @@ from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from mycraze.models.user.profile import UserProfile
+from mycraze.services.search import UserSearchService
 
 @login_required
 def get_users_page(request):
-    criteria=""
-    q=""
+    criteria=None
+    q=None
     if request.GET.get('criteria'):
         criteria=request.GET.get('criteria')
     else:
         return HttpResponseRedirect("/mycraze/users/?criteria=all")
     if request.GET.get('q'):
         q=request.GET.get('q')
-        
     
+    user_list = None    
+    if criteria == 'all':
+        user_list=UserSearchService.get_all_users(q)
+    elif criteria == 'people':
+        user_list=UserSearchService.get_users_by_name(q)
+    elif criteria == 'skill':
+        user_list=UserSearchService.get_users_by_skills(q)
     context = {
         'image_url': settings.PROFILE_IMAGES_URL,
-        'user_list': UserProfile.objects.all()
+        'query':q,
+        'user_list': user_list
     }
     return render(request, 'mycraze/users.html',context)
