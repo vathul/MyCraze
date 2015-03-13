4c08f7892647e7b5ffef97adf17d51eab5ed9f82
Srikrishnan S <srikrishnans@sase.ssn.edu.in>
2015-03-13 18:55:19 +0530

    Selecting distinct results for the all query set

--- sase/mycraze/services/search.py
+++ sase/mycraze/services/search.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.db.models import Q
 
 class UserSearchService:
-    
+
     @staticmethod
     def get_users_by_name(query):
         user_list = None
@@ -27,5 +27,5 @@ class UserSearchService:
         if query == None:
             user_list = User.objects.filter(Q(user_profile__isnull=False))
         else:
-            user_list = User.objects.filter(Q(first_name__icontains=query) | Q(last_name__icontains=query) | Q(user_profile__skill_section__skill_items__skill__icontains=query))
+            user_list = User.objects.filter(Q(first_name__icontains=query) | Q(last_name__icontains=query) | Q(user_profile__skill_section__skill_items__skill__icontains=query)).distinct()
         return user_list
