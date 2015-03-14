7df248ae58511f29125a98fad7c1a39360d03000
Srikrishnan S <srikrishnans@sase.ssn.edu.in>
2015-03-14 20:02:41 +0530

    Adding links to the side navigation pane in the users page

--- sase/mycraze/services/__init__.py
+++ sase/mycraze/services/__init__.py
@@ -1,4 +1,4 @@
 from mycraze.services.user import UserProfileService
+from mycraze.services.search import UserSearchService
 
-
-__all__ = ['UserProfileService']
+__all__ = ['UserProfileService','UserSearchService']
