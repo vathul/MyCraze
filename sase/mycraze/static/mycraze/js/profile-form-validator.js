c0dbed27a9f26b96c962d529dd0e80cdd66c0b44
Srikrishnan S <srikrishnans@sase.ssn.edu.in>
2015-03-19 01:18:59 +0530

    Adding the validations for the profile completion page

--- sase/mycraze/static/mycraze/js/profile-form-validator.js
+++ sase/mycraze/static/mycraze/js/profile-form-validator.js
@@ -1,3 +1,12 @@
-$(document).on('submit','form.save-profile',function(){
-
+$("#save-profile").submit(function() {
+	var hasNoError = true;
+    var $inputs = $("#save-profile :input").not(':button,:hidden');
+	$inputs.each(function() {
+		$(this).parent().removeClass('has-error');
+		if($(this).val() == "") {
+			$(this).parent().addClass('has-error');
+			hasNoError = false;
+		}
+	});
+	return hasNoError;
 });
