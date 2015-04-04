a03b83cdc5f6c3efa60f6521361c61a7c9e3d2f8
Srikrishnan S <srikrishnans@sase.ssn.edu.in>
2015-04-04 22:36:01 +0530

    Adding python docstrings

--- sase/mycraze/models/user/items.py
+++ sase/mycraze/models/user/items.py
@@ -1,3 +1,10 @@
+"""This module contains model classes pertaining to various items in the user
+resume page"""
+
+__author__ = "Srikrishnan Suresh"
+__copyright__ = "Copyright 2015, MyCraze"
+__version__ = "1.0.1"
+
 from django.db import models
 
 from mycraze.models.user.sections import AwardSection
@@ -12,76 +19,130 @@ from mycraze.models.user.sections import SkillSection
 
 
 class ExperienceItem(models.Model):
-	class Meta:
-		app_label = 'mycraze'
+    """This module is the model class that represents an experience item within
+    an experience section in user resume.
+    Refer py:module:: mycraze.models.user.sections.ExperienceSection
+    Can be accessed as experience_section.experience_items.
+    """
 
-	role = models.CharField(max_length=40)
-	organization = models.CharField(max_length=60)
-	description = models.CharField(max_length=400)
-	experience_section = models.ForeignKey(ExperienceSection, related_name="experience_items")
+    class Meta:
+        app_label = 'mycraze'
+
+    role = models.CharField(max_length=40)
+    organization = models.CharField(max_length=60)
+    description = models.CharField(max_length=400)
+    experience_section = models.ForeignKey(ExperienceSection, related_name="experience_items")
 
 class ProjectItem(models.Model):
-	class Meta:
-		app_label = 'mycraze'
+    """This module is the model class that represents a project item within
+    a project section in user resume.
+    Refer py:module:: mycraze.models.user.sections.ProjectSection
+    Can be accessed as project_section.project_items.
+    """
 
-	title = models.CharField(max_length=100)
-	url = models.CharField(max_length=200)
-	description = models.CharField(max_length=400)
-	project_section = models.ForeignKey(ProjectSection, related_name="project_items")
+    class Meta:
+        app_label = 'mycraze'
+
+    title = models.CharField(max_length=100)
+    url = models.CharField(max_length=200)
+    description = models.CharField(max_length=400)
+    project_section = models.ForeignKey(ProjectSection, related_name="project_items")
 
 class EducationItem(models.Model):
-	class Meta:
-		app_label = 'mycraze'
+    """This module is the model class that represents an education item within
+    an education section in user resume.
+    Refer py:module:: mycraze.models.user.sections.EducationSection
+    Can be accessed as education_section.education_items.
+    """
 
-	school = models.CharField(max_length=200)
-	degree = models.CharField(max_length=100)
-	description = models.CharField(max_length=400)
-	education_section = models.ForeignKey(EducationSection, related_name="education_items")
+    class Meta:
+        app_label = 'mycraze'
+
+    school = models.CharField(max_length=200)
+    degree = models.CharField(max_length=100)
+    description = models.CharField(max_length=400)
+    education_section = models.ForeignKey(EducationSection, related_name="education_items")
 
 class PublicationItem(models.Model):
-	class Meta:
-		app_label = 'mycraze'
+    """This module is the model class that represents a publication item within
+    a publication section in user resume.
+    Refer py:module:: mycraze.models.user.sections.PublicationSection
+    Can be accessed as publication_section.publication_items.
+    """
 
-	title = models.CharField(max_length=200)
-	publisher = models.CharField(max_length=100)
-	description = models.CharField(max_length=400)
-	publication_section = models.ForeignKey(PublicationSection, related_name="publication_items")
+    class Meta:
+        app_label = 'mycraze'
+
+    title = models.CharField(max_length=200)
+    publisher = models.CharField(max_length=100)
+    description = models.CharField(max_length=400)
+    publication_section = models.ForeignKey(PublicationSection, related_name="publication_items")
 
 class CertificationItem(models.Model):
-	class Meta:
-		app_label = 'mycraze'
+    """This module is the model class that represents a certification item within
+    a certification section in user resume.
+    Refer py:module:: mycraze.models.user.sections.CertificationSection
+    Can be accessed as certification_section.certification_items.
+    """
 
-	title = models.CharField(max_length=200)
-	certifier = models.CharField(max_length=100)
-	certification_section = models.ForeignKey(CertificationSection, related_name="certification_items")
+    class Meta:
+        app_label = 'mycraze'
+
+    title = models.CharField(max_length=200)
+    certifier = models.CharField(max_length=100)
+    certification_section = models.ForeignKey(CertificationSection, related_name="certification_items")
 
 class AwardItem(models.Model):
-	class Meta:
-		app_label = 'mycraze'
+    """This module is the model class that represents an award item within
+    an award section in user resume.
+    Refer py:module:: mycraze.models.user.sections.AwardSection
+    Can be accessed as award_section.award_items.
+    """
 
-	title = models.CharField(max_length=200)
-	issuer = models.CharField(max_length=100)
-	description = models.CharField(max_length=400)
-	award_section = models.ForeignKey(AwardSection, related_name="award_items")
+    class Meta:
+        app_label = 'mycraze'
+
+    title = models.CharField(max_length=200)
+    issuer = models.CharField(max_length=100)
+    description = models.CharField(max_length=400)
+    award_section = models.ForeignKey(AwardSection, related_name="award_items")
 
 class SkillItem(models.Model):
-	class Meta:
-		app_label = 'mycraze'
+    """This module is the model class that represents a skill item within
+    a skill section in user resume.
+    Refer py:module:: mycraze.models.user.sections.SkillSection
+    Can be accessed as skill_section.skill_items.
+    """
 
-	skill = models.CharField(max_length=100)
-	skill_section = models.ForeignKey(SkillSection, related_name="skill_items")
+    class Meta:
+        app_label = 'mycraze'
+
+    skill = models.CharField(max_length=100)
+    skill_section = models.ForeignKey(SkillSection, related_name="skill_items")
 
 class CourseItem(models.Model):
-	class Meta:
-		app_label = 'mycraze'
+    """This module is the model class that represents a course item within
+    a course section in user resume.
+    Refer py:module:: mycraze.models.user.sections.CourseSection
+    Can be accessed as course_section.course_items.
+    """
 
-	title = models.CharField(max_length=100)
-	code = models.CharField(max_length=20)
-	course_section = models.ForeignKey(CourseSection, related_name="course_items")
+    class Meta:
+        app_label = 'mycraze'
+
+    title = models.CharField(max_length=100)
+    code = models.CharField(max_length=20)
+    course_section = models.ForeignKey(CourseSection, related_name="course_items")
 
 class LanguageItem(models.Model):
-	class Meta:
-		app_label = 'mycraze'
+    """This module is the model class that represents a language item within
+    a language section in user resume.
+    Refer py:module:: mycraze.models.user.sections.LanguageSection
+    Can be accessed as language_section.language_items.
+    """
 
-	language = models.CharField(max_length=100)
-	language_section = models.ForeignKey(LanguageSection, related_name="language_items")
+    class Meta:
+        app_label = 'mycraze'
+
+    language = models.CharField(max_length=100)
+    language_section = models.ForeignKey(LanguageSection, related_name="language_items")
