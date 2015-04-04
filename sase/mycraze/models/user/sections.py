1829a6c2b9fdf928bf8487344b36df7374123041
Srikrishnan S <srikrishnans@sase.ssn.edu.in>
2015-04-04 23:21:15 +0530

    Adding python docstrings

--- sase/mycraze/models/user/sections.py
+++ sase/mycraze/models/user/sections.py
@@ -1,9 +1,22 @@
+"""This module contains classes pertaining to the user's section is his resume.
+All these classes have one-to-one relationship with UserProfile class."""
+
+__author__ = "Srikrishnan Suresh"
+__copyright__ = "Copyright 2015, MyCraze"
+__version__ = "1.0.1"
+
 from django.db import models
 
 from mycraze.models.user.profile import UserProfile
 
 
 class SummarySection(models.Model):
+	"""This module is the model class that represents user's summary section
+	in the resume.
+    Refer py:mycraze.models.user.profile.UserProfile
+    Can be accessed by user_profile.summary_section.
+    """
+
 	class Meta:
 		app_label = 'mycraze'
 
@@ -12,6 +25,12 @@ class SummarySection(models.Model):
 	is_active = models.BooleanField(default=True)
 
 class ProjectSection(models.Model):
+	"""This module is the model class that represents user's project section
+	in the resume.
+    Refer py:mycraze.models.user.profile.UserProfile
+    Can be accessed by user_profile.project_section.
+    """
+
 	class Meta:
 		app_label = 'mycraze'
 
@@ -19,6 +38,12 @@ class ProjectSection(models.Model):
 	is_active = models.BooleanField(default=True)
 
 class ExperienceSection(models.Model):
+	"""This module is the model class that represents user's experience section
+	in the resume.
+    Refer py:mycraze.models.user.profile.UserProfile
+    Can be accessed by user_profile.experience_section.
+    """
+
 	class Meta:
 		app_label = 'mycraze'
 
@@ -26,6 +51,12 @@ class ExperienceSection(models.Model):
 	is_active = models.BooleanField(default=True)
 
 class EducationSection(models.Model):
+	"""This module is the model class that represents user's education section
+	in the resume.
+    Refer py:mycraze.models.user.profile.UserProfile
+    Can be accessed by user_profile.education_section.
+    """
+
 	class Meta:
 		app_label = 'mycraze'
 
@@ -33,6 +64,12 @@ class EducationSection(models.Model):
 	is_active = models.BooleanField(default=True)
 
 class ProfileSection(models.Model):
+	"""This module is the model class that represents user's profile section
+	in the resume.
+    Refer py:mycraze.models.user.profile.UserProfile
+    Can be accessed by user_profile.profile_section.
+    """
+
 	class Meta:
 		app_label = 'mycraze'
 
@@ -40,6 +77,12 @@ class ProfileSection(models.Model):
 	is_active = models.BooleanField(default=True)
 
 class PublicationSection(models.Model):
+	"""This module is the model class that represents user's publication section
+	in the resume.
+    Refer py:mycraze.models.user.profile.UserProfile
+    Can be accessed by user_profile.publication_section.
+    """
+
 	class Meta:
 		app_label = 'mycraze'
 
@@ -47,6 +90,12 @@ class PublicationSection(models.Model):
 	is_active = models.BooleanField(default=True)
 
 class CertificationSection(models.Model):
+	"""This module is the model class that represents user's certification section
+	in the resume.
+    Refer py:mycraze.models.user.profile.UserProfile
+    Can be accessed by user_profile.certification_section.
+    """
+
 	class Meta:
 		app_label = 'mycraze'
 
@@ -54,6 +103,12 @@ class CertificationSection(models.Model):
 	is_active = models.BooleanField(default=True)
 
 class SkillSection(models.Model):
+	"""This module is the model class that represents user's skill section
+	in the resume.
+    Refer py:mycraze.models.user.profile.UserProfile
+    Can be accessed by user_profile.skill_section.
+    """
+
 	class Meta:
 		app_label = 'mycraze'
 
@@ -61,6 +116,12 @@ class SkillSection(models.Model):
 	is_active = models.BooleanField(default=True)
 
 class CourseSection(models.Model):
+	"""This module is the model class that represents user's course section
+	in the resume.
+    Refer py:mycraze.models.user.profile.UserProfile
+    Can be accessed by user_profile.course_section.
+    """
+
 	class Meta:
 		app_label = 'mycraze'
 
@@ -68,6 +129,12 @@ class CourseSection(models.Model):
 	is_active = models.BooleanField(default=True)
 
 class AwardSection(models.Model):
+	"""This module is the model class that represents user's award section
+	in the resume.
+    Refer py:mycraze.models.user.profile.UserProfile
+    Can be accessed by user_profile.award_section.
+    """
+
 	class Meta:
 		app_label = 'mycraze'
 
@@ -75,6 +142,12 @@ class AwardSection(models.Model):
 	is_active = models.BooleanField(default=True)
 
 class LanguageSection(models.Model):
+	"""This module is the model class that represents user's language section
+	in the resume.
+    Refer py:mycraze.models.user.profile.UserProfile
+    Can be accessed by user_profile.language_section.
+    """
+
 	class Meta:
 		app_label = 'mycraze'
 
@@ -82,6 +155,12 @@ class LanguageSection(models.Model):
 	is_active = models.BooleanField(default=True)
 
 class ContactSection(models.Model):
+	"""This module is the model class that represents user's contact section
+	in the resume.
+    Refer py:mycraze.models.user.profile.UserProfile
+    Can be accessed by user_profile.contact_section.
+    """
+
 	class Meta:
 		app_label = 'mycraze'
 
