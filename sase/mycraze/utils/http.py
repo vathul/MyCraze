9d196c0db0c685a91e99fd8b6dc975e9f606c341
Srikrishnan S <srikrishnans@sase.ssn.edu.in>
2015-04-05 09:15:21 +0530

    Adding python docstrings

--- sase/mycraze/utils/http.py
+++ sase/mycraze/utils/http.py
@@ -1,10 +1,15 @@
+"""This module contains util classes related to web http."""
+
+__author__ = "Srikrishnan Suresh"
+__copyright__ = "Copyright 2015, MyCraze"
+__version__ = "1.0.1"
+
 from django.http import HttpResponse
 from django.utils import simplejson
 
 
 class JsonResponse(HttpResponse):
-    """
-        JSON response
+    """JSON response class, used to render an HTML response in json format.
     """
     def __init__(self, content, mimetype='application/json', status=None, content_type=None):
         super(JsonResponse, self).__init__(
