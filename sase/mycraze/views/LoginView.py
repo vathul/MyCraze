bf788bf2fc2bb019438ef2c516943d451c9bc2d1
Srikrishnan S <srikrishnans@sase.ssn.edu.in>
2015-04-05 09:32:21 +0530

    Adding python docstrings

--- sase/mycraze/views/LoginView.py
+++ sase/mycraze/views/LoginView.py
@@ -1,3 +1,10 @@
+"""This module contains view classes pertaining to login, sign up and profile
+completion."""
+
+__author__ = "Srikrishnan Suresh"
+__copyright__ = "Copyright 2015, MyCraze"
+__version__ = "1.0.1"
+
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
@@ -6,9 +13,12 @@ from django.shortcuts import render
 from mycraze.models.form.profile import UserForm
 from mycraze.models.form.profile import UserProfileForm
 
-
-# Create your login views here.
 def get_login_page(request):
+    """This function returns the login/sign up page view if the user is
+    not authenticated yet. If an authenticated user enters, the user is
+    redirected to resume page. 
+    """
+
     if request.user.is_authenticated():
         response_url = '/mycraze/user-resume/' + str(request.user.id)
         return HttpResponseRedirect(response_url)
@@ -17,6 +27,10 @@ def get_login_page(request):
 
 @login_required
 def get_profile_completion_page(request):
+    """This function returns the profile completion page view with the form
+    for the user to fill his profile information. 
+    """
+
     data = {'first_name': request.user.first_name,
             'last_name': request.user.last_name}
     user_form = UserForm(data)
@@ -27,5 +41,8 @@ def get_profile_completion_page(request):
     })
 
 def get_logout_page(request):
+    """This function logs out the user and redirects him to logout page. The
+    cookies created by Django are not cleared during logout.  
+    """
     logout(request)
     return render(request, 'mycraze/logout.html')
--- sase/mycraze/views/TemplateView.py
+++ sase/mycraze/views/TemplateView.py
@@ -1,3 +1,9 @@
+"""This module contains view classe to receive user ping on the app."""
+
+__author__ = "Srikrishnan Suresh"
+__copyright__ = "Copyright 2015, MyCraze"
+__version__ = "1.0.1"
+
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
@@ -5,5 +11,7 @@ from django.shortcuts import render
 # Create your views here.
 @login_required
 def get_template_page(request):
+	"""This function returns a ping, to indicate the app is healthy. 
+    """
 	context = {'message': 'The site is healthy'}
 	return render(request, 'mycraze/template.html', context)
